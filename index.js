1863293cb77202239246626c78fb706bad1929e1
mrtavvv <suo.ishikawa@gmail.com>
2025-02-13 16:12:43 +0800

    changes

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+  // Unmute the audio when the user clicks anywhere on the document
+document.addEventListener('click', function unmuteAudio() {
+    var audio = document.getElementById('bg-audio');
+    if (audio.muted) {
+      audio.muted = false;
+      audio.play();
+    }
+    // Optionally, remove the event listener after unmuting
+    document.removeEventListener('click', unmuteAudio);
+  });
